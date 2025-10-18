32ed1dde1a08ee0bfa3f78e560086262a2fc38b0
RematchGamer <139418967+RematchGamer@users.noreply.github.com>
2025-10-18 13:27:18 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,18 +1,20 @@
-local p = game.Players.LocalPlayer
-local click = require(p.PlayerScripts.PlayerModule):GetClickToMoveController()
+local player = game.Players.LocalPlayer
+local clickToMove = require(player.PlayerScripts.PlayerModule):GetClickToMoveController()
 local barrier = workspace:FindFirstChild("Barrier")
 if barrier then barrier:Destroy() end
 
-local gui = Instance.new("ScreenGui", p:WaitForChild("PlayerGui"))
-local btn = Instance.new("TextButton", gui)
-btn.Size = UDim2.new(0,100,0,50)
-btn.Position = UDim2.new(0,50,0,50)
-btn.Text = "Stop"
+local gui = Instance.new("ScreenGui", player:WaitForChild("PlayerGui"))
+local endButton = Instance.new("TextButton", gui)
+endButton.Size = UDim2.new(0,100,0,50)
+endButton.Position = UDim2.new(0,10,1,-60) -- bottom left
+endButton.Text = "Stop"
 
 local running = true
-btn.MouseButton1Click:Connect(function() running = false end)
+endButton.MouseButton1Click:Connect(function()
+	running = false
+end)
 
-local prof = game.ReplicatedStorage.Profiles[p.Name]
+local prof = game.ReplicatedStorage.Profiles[player.Name]
 local mobFolder = prof:FindFirstChild("Mobs")
 
 local mobCases = {
@@ -20,16 +22,20 @@ local mobCases = {
 	["Rekindled Unborn"] = {c1=Vector3.new(200,0,0), c2=Vector3.new(0,0,0)}
 }
 
-local mob, c1, c2
+local selectedMob, c1, c2
 for name,data in pairs(mobCases) do
 	if mobFolder:FindFirstChild(name) then
-		mob,c1,c2=name,data.c1,data.c2
+		selectedMob, c1, c2 = name,data.c1,data.c2
 		break
 	end
 end
-if not mob then return end
+if not selectedMob then return end
 
 while running and task.wait(1) do
-	if mobFolder:FindFirstChild(mob) then click:MoveTo(c1)
-	else click:MoveTo(c2) end
+	local mobFound = mobFolder:FindFirstChild(selectedMob)
+	if mobFound then
+		clickToMove:MoveTo(c1)
+	else
+		clickToMove:MoveTo(c2)
+	end
 end
