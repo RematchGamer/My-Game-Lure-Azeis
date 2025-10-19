15f915a39e6b0c5cf77618270d57fbf6ba454adc
RematchGamer <139418967+RematchGamer@users.noreply.github.com>
2025-10-18 20:04:21 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -14,29 +14,37 @@ endButton.MouseButton1Click:Connect(function()
 	running = false
 end)
 
-local profileMobs = game.ReplicatedStorage.Profiles[player.Name].Mobs
-
 local mobCases = {
 	["Azeis, Spirit of the Eternal Blossom"] = {c1=Vector3.new(-45,48,-42), c2=Vector3.new(37,48,-54)},
 	["Rekindled Unborn"] = {c1=Vector3.new(200,0,0), c2=Vector3.new(0,0,0)}
 }
 
 local selectedMob, c1, c2
-for name,data in pairs(mobCases) do
-	if profileMobs:FindFirstChild(name) then
-		selectedMob, c1, c2 = name,data.c1,data.c2
-		break
+local timeout = 120
+local startTime = tick()
+
+-- Wait until any mob in workspace.Mobs exists (or 120 seconds)
+while running and tick() - startTime < timeout and not selectedMob do
+	for name, data in pairs(mobCases) do
+		if workspace.Mobs:FindFirstChild(name) then
+			selectedMob, c1, c2 = name, data.c1, data.c2
+			break
+		end
 	end
+	task.wait(0.5)
 end
 
-local setInterval = 1
-while running and selectedMob and task.wait(setInterval) do
-	local mobFound = workspace.Mobs:FindFirstChild(selectedMob)
-	if mobFound then
-		clickToMove:MoveTo(c1)
-	else
-		clickToMove:MoveTo(c2)
+if selectedMob then
+	local setInterval = 1
+	while running and task.wait(setInterval) do
+		local mobFound = workspace.Mobs:FindFirstChild(selectedMob)
+		if mobFound then
+			clickToMove:MoveTo(c1)
+		else
+			clickToMove:MoveTo(c2)
+		end
 	end
 end
-
+warn("No mob found within 120 seconds")
 gui:Destroy()
+return
