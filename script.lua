56f2b179fa9917b0518bd1ab973901aed207feb8
RematchGamer <139418967+RematchGamer@users.noreply.github.com>
2025-10-19 07:13:17 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -3,50 +3,62 @@ local clickToMove = require(player.PlayerScripts.PlayerModule):GetClickToMoveCon
 
 workspace:FindFirstChild("Barrier"):Destroy()
 
-local gui = Instance.new("ScreenGui", player:WaitForChild("PlayerGui"))
-local endButton = Instance.new("TextButton", gui)
-endButton.Size = UDim2.new(0,100,0,50)
-endButton.Position = UDim2.new(0,10,1,-60)
-endButton.Text = "Stop"
+-- Load your GUI library
+local library = loadstring(game:HttpGet("https://gist.githubusercontent.com/oufguy/62dbf2a4908b3b6a527d5af93e7fca7d/raw/6b2a0ecf0e24bbad7564f7f886c0b8d727843a92/Swordburst%25202%2520KILL%2520AURA%2520GUI(not%2520script)"))()
+local window = library:MakeWindow("Mob Selector")
 
+-- Hardcoded mobs and positions
+local mobCases = {
+	["Azeis, Spirit of the Eternal Blossom"] = {c1 = Vector3.new(-45,48,-42), c2 = Vector3.new(37,48,-54)},
+	["Rekindled Unborn"] = {c1 = Vector3.new(200,0,0), c2 = Vector3.new(0,0,0)}
+}
+
+-- Keep track of selected mob
+local selectedMob, c1, c2
 local running = true
-endButton.MouseButton1Click:Connect(function()
+
+-- Stop button
+window:addButton("Stop", function()
 	running = false
 end)
 
-local mobCases = {
-	["Azeis, Spirit of the Eternal Blossom"] = {c1=Vector3.new(-45,48,-42), c2=Vector3.new(37,48,-54)},
-	["Rekindled Unborn"] = {c1=Vector3.new(200,0,0), c2=Vector3.new(0,0,0)}
-}
-
-local selectedMob, c1, c2
-local timeout = 120
-local startTime = tick()
-
--- Wait until any mob in workspace.Mobs exists (or 120 seconds)
-while running and tick() - startTime < timeout and not selectedMob do
-	for name, data in pairs(mobCases) do
-		if workspace.Mobs:FindFirstChild(name) then
-			selectedMob, c1, c2 = name, data.c1, data.c2
-			break
-		end
-	end
-	task.wait(0.5)
-end
-
-if selectedMob then
-	local setInterval = 1
-	while running and task.wait(setInterval) do
-		local mobFound = workspace.Mobs:FindFirstChild(selectedMob)
-		if mobFound then
-			clickToMove:MoveTo(c1)
+-- Checkbox list for mob selection
+local checkboxes = {}
+for name,_ in pairs(mobCases) do
+	local cb = window:addCheckbox(name)
+	cb.Checked.Changed:Connect(function()
+		if cb.Checked.Value then
+			selectedMob = name
+			c1 = mobCases[name].c1
+			c2 = mobCases[name].c2
+			-- uncheck other checkboxes
+			for otherName, otherCb in pairs(checkboxes) do
+				if otherName ~= name then
+					otherCb.Checked.Value = false
+				end
+			end
 		else
-			clickToMove:MoveTo(c2)
+			if selectedMob == name then
+				selectedMob = nil
+				c1 = nil
+				c2 = nil
+			end
 		end
-	end
-else
-	warn("No mob found within 120 seconds")
-end
+	end)
+	checkboxes[name] = cb
+end)
 
-gui:Destroy()
-return
+-- Main loop
+spawn(function()
+	while running do
+		if selectedMob then
+			local mobFound = workspace.Mobs:FindFirstChild(selectedMob)
+			if mobFound then
+				clickToMove:MoveTo(c1)
+			else
+				clickToMove:MoveTo(c2)
+			end
+		end
+		task.wait(1)
+	end
+end)
